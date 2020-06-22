3d7c9940d9f412dd49faa2f311936975c1c72a6c
Jason Anthony <janthony@jsaonline.com>
2020-06-22 09:28:03 -0500

    installed jsonwebtoken

--- app_modules/auth.js
+++ app_modules/auth.js
@@ -1,6 +1,12 @@
 "use strict"
+const jwt = require('express-jwt');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
+    try{
+        
+        return jwt({ secret: 'shhhhhhared-secret'});
 
-    
+    }catch(err){
+        console.log(err);
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    auth = require('../app_modules/auth');
 
 router.get('/', (req,res) => {
     res.send('Hello World')
+})
+
+router.get('/protected', auth, (req, res) => {
+    res.send('Ok');
 })
 
 module.exports = router;
