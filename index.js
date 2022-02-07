99528ff4e2c8839ae8b7929b73f5fca863919f24
Jason Anthony <janthony@jsaonline.com>
2022-02-07 07:51:07 -0500

    updated router for new route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var routes = require('./routes')
+const express = require('express');
+const app = express();
+const port = 9000;
+const routes = require('./routes');
 app.use(routes);
-
-app.listen(port, ()=> { console.log(`app listening on http://localhost:${port}`); })
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send('invalid token...');
+    }
+});
+app.listen(port, ()=> { console.log(`app listening on http://localhost:${port}`); });
